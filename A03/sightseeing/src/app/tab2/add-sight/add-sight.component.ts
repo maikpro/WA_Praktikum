be4a7d1cdf023fca473ffe3aaac8e920e98cb7a1
Maik <maik.proba@gmail.com>
2022-05-03 18:22:22 +0200

    show preview of image after selection

--- A03/sightseeing/src/app/tab2/add-sight/add-sight.component.ts
+++ A03/sightseeing/src/app/tab2/add-sight/add-sight.component.ts
@@ -1,6 +1,9 @@
 /* eslint-disable @typescript-eslint/quotes */
 /* eslint-disable no-trailing-spaces */
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import { Directory, Filesystem } from '@capacitor/filesystem';
 import { ModalController } from '@ionic/angular';
 import { GpsPostion } from 'src/app/models/gps-position';
 
@@ -10,13 +13,38 @@ import { GpsPostion } from 'src/app/models/gps-position';
   styleUrls: ['./add-sight.component.scss'],
 })
 export class AddSightComponent implements OnInit {
+  public sightForm!: FormGroup;
 
-  constructor(private modalController: ModalController) { }
+  public photo: Photo;
 
-  public ngOnInit() {}
+  constructor(private modalController: ModalController, private formBuilder: FormBuilder) { }
+
+  public ngOnInit() {
+    this.sightForm = this.formBuilder.group({
+      name: ['', Validators.required],
+    });
+  }
 
   public async closeModal(){
     await this.modalController.dismiss(); // Schließt das Modal
+  }
+
+  public async selectImage() {
+    this.photo = await Camera.getPhoto({
+      quality: 100,
+      allowEditing: false,
+      resultType: CameraResultType.Base64,
+      source: CameraSource.Photos // TODO: Hier auf Camera wechseln!
+    });
+
+    console.log(this.photo);
+    console.log("PATH: ", this.photo.path);
+  }
+
+  public displayImage(): string {
+    if(this.photo){
+      return 'data:image/jpeg;base64,' + this.photo.base64String;
+    }
   }
 
   public save(): void {
@@ -32,6 +60,22 @@ export class AddSightComponent implements OnInit {
       gpsPosition
     };
 
+    if(this.photo){
+      this.saveImage();
+    }
+
     this.modalController.dismiss(testSight);
+
+  }
+
+  private async saveImage(){
+    const fileName = 'sightseeing_' + new Date().getTime() + '.jpeg';
+    const savedFile = await Filesystem.writeFile({
+      directory: Directory.Data, // Saves in data Application folder => when app uninstalled photos deleted!
+      path: `images/${fileName}`,
+      data: this.photo.base64String
+    });
+
+    console.log("saved File: ", savedFile);
   }
 }
